78557f99a22626a8d726ac00ffd4bbaa03e26f00
joshmg-CA <joshmg276@gmail.com>
2021-04-01 10:20:59 -0600

    Details

--- src/Components/Head/windowPopups/settings.js
+++ src/Components/Head/windowPopups/settings.js
@@ -151,7 +151,7 @@ export default function Settings({ openItem, onBackInner }) {
   useEffect(() => {
     /* with context we pass all data to the context and we're cosume in every*/
    setWaterMark({...WaterMark,Mark:options.watermark})
-  }, [...Object.keys(options).map(e=>e)]);
+  }, [...Object.keys(options).map(e=>{return "options."+e})]);
 
   useEffect(() => {
     setOptions({ ...options, color: randoms });
